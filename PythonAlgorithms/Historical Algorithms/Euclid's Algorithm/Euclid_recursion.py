9d1c13ff77923a133660adb8bd4f977128bb218a
Sephew <146841266+Sephew@users.noreply.github.com>
2024-09-04 00:27:31 +0800

    Update Euclid_recursion.py
    added comment

--- PythonAlgorithms/Historical Algorithms/Euclid's Algorithm/Euclid_recursion.py
+++ PythonAlgorithms/Historical Algorithms/Euclid's Algorithm/Euclid_recursion.py
@@ -1,5 +1,9 @@
 import math
-
+# Although Euclid made alot of algorithms, his most famouse one is the algorithm to find the greatest common denominator
+# A = q1 * B + C
+# B = q2 * C + D
+# C = q3 * D + E
+# so on and so fort until the trailing constant becomes 0.
 def func(x,y):
     smaller = min(x,y)
     larger = max(x,y)
