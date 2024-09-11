b2cd44e4d2ecd0a682d9926b463c7d6388981e81
Sephew <146841266+Sephew@users.noreply.github.com>
2024-09-11 23:37:11 +0800

    Update Atm.java
    edited the indentation*

--- ATM System/Atm.java
+++ ATM System/Atm.java
@@ -1,3 +1,4 @@
+
 /**
  * ATM Machine System
     user is perform the following operations
@@ -11,55 +12,51 @@ import java.util.Scanner;
 
 public class Atm {
 
-
-    private static void PressToContinue(Scanner scanner){
+    private static void PressToContinue(Scanner scanner) {
         System.out.println("Press Enter to Continue...");
-        scanner.nextLine(); //absorbs the "\n" from nextInt();
-        scanner.nextLine(); //absorbs the Enter key;
+        scanner.nextLine(); // absorbs the "\n" from nextInt();
+        scanner.nextLine(); // absorbs the Enter key;
 
     }
+
     public static void main(String[] args) {
 
-        //On and Off switch of the program
+        // On and Off switch of the program
         boolean program = true;
-        
-        //initialize
+
+        // initialize
         AccountData User = new AccountData();
         Scanner _input = new Scanner(System.in);
-        
 
+        // LogIn System:
 
-        //LogIn System:
-
-        //enter the acc num to test
+        // enter the acc num to test
         // System.out.println("Enter Account Name: ");
-        //     StringBuilder accountnum;
-        //     accountnum.append(_input.nextLine());
-        //     //initialize databank
-        //     String _filename = "names.txt";
-            
-            
+        // StringBuilder accountnum;
+        // accountnum.append(_input.nextLine());
+        // //initialize databank
+        // String _filename = "names.txt";
 
+        do {
 
-        do{
-
-            //User.name = accountnum;
-            System.out.println("<-----JAVA ATM MACHINE----->\nSelect an Option:\n1. Check Balance\n2. Deposit\n3. Withdraw\n4. Exit\nChoice:");
+            // User.name = accountnum;
+            System.out.println(
+                    "<-----JAVA ATM MACHINE----->\nSelect an Option:\n1. Check Balance\n2. Deposit\n3. Withdraw\n4. Exit\nChoice:");
 
             int choice = _input.nextInt();
-            switch(choice){
+            switch (choice) {
                 case 1:
-                    //check balance
+                    // check balance
                     User.checkBalance();
                     PressToContinue(_input);
                     break;
                 case 2:
-                    //deposit
+                    // deposit
                     System.out.println("Enter Deposit Value: ");
-                    try{
+                    try {
                         int Depositvalue = _input.nextInt();
                         User.deposit(Depositvalue);
-                    }catch(InputMismatchException e){
+                    } catch (InputMismatchException e) {
                         System.out.println("Error, Please input whole numbers only,");
                         _input.next();
                     }
@@ -67,29 +64,28 @@ public class Atm {
                     PressToContinue(_input);
                     break;
                 case 3:
-                    //withdraw
+                    // withdraw
                     System.out.println("Enter Withdraw Value: ");
 
-                    try{
+                    try {
                         int Withdrawvalue = _input.nextInt();
                         User.withdraw(Withdrawvalue);
-                    }catch(InputMismatchException e){
+                    } catch (InputMismatchException e) {
                         System.out.println("Error, Please input whole numbers only,");
                         _input.next();
                     }
 
-
                     PressToContinue(_input);
                     break;
                 case 4:
-                    //exit
+                    // exit
                     System.out.println("Thank you! See you Again!");
-                    _input.close(); //closes the scanner
+                    _input.close(); // closes the scanner
                     program = false;
-                
+
             }
-            
-        }while(program);
+
+        } while (program);
         return;
     }
 }
